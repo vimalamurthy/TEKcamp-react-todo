d7a645462371e7f2c388037c2892571e9f7cf306
Vimala Murthy <vimala_murthy@yahoo.com>
2020-06-04 18:42:41 -0500

    Working Todo

--- react-todo/src/Todo.jsx
+++ react-todo/src/Todo.jsx
@@ -1,4 +1,95 @@
 import React, { useState } from 'react';
+import './Todo.css';
 
-export default function Todo() {
+function CreateItem({ addItem }) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (value) {
+      addItem(value);
+      setValue('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        className='input'
+        value={value}
+        placeholder='Add to the list and ENTER..'
+        onChange={(event) => setValue(event.target.value)}
+      />
+    </form>
+  );
 }
+
+function Item({ item, index, finishItem, deleteItem }) {
+  if (index === 0) return <div></div>;
+  else
+    return (
+      <div
+        className='item'
+        style={{ textDecoration: item.completed ? 'line-through' : '' }}
+      >
+        {item.title}
+        <button onClick={() => deleteItem(index)}>X</button>
+        <button onClick={() => finishItem(index)}>Done</button>
+      </div>
+    );
+}
+
+function Todo() {
+  const [items, setItems] = useState([{}]);
+
+  const addItem = (title) => {
+    const newItems = [...items, { title, completed: false }];
+    setItems(newItems);
+  };
+
+  const finishItem = (index) => {
+    const newItems = [...items];
+    newItems[index].completed = true;
+    setItems(newItems);
+  };
+
+  const deleteItem = (index) => {
+    const newItems = [...items];
+    newItems.splice(index, 1);
+    setItems(newItems);
+  };
+
+  const deleteAllItem = (index) => {
+    const newItems = [...items];
+    newItems.splice(1, items.length);
+    setItems(newItems);
+  };
+
+  return (
+    <div className='todo-container'>
+      <h1>TO DO LIST</h1>
+      <div>
+        {items.map((item, index) => (
+          <Item
+            item={item}
+            index={index}
+            finishItem={finishItem}
+            deleteItem={deleteItem}
+            key={index}
+          />
+        ))}
+      </div>
+      <div className='create-item'>
+        <CreateItem addItem={addItem} />
+      </div>
+      <div>
+        <button className='delete-all' onClick={deleteAllItem}>
+          Delete All
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
